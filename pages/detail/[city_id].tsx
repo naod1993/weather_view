e883e612734816d3e4b25cd41c672fe609ebf8c9
naod1993 <nfisseha23@wooster.edu>
2024-03-01 05:39:57 -0800

    add image and image domain

--- pages/detail/[city_id].tsx
+++ pages/detail/[city_id].tsx
@@ -4,6 +4,8 @@ import cities from "@/lib/city.list.json";
 import { GetServerSidePropsContext } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image";
+
 let Cities = cities as CityData[];
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const { city_id } = context.query;
@@ -12,8 +14,7 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   if (!city) {
     throw new Error("City not found");
   }
-  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${city.coord.lat}&lon=${city.coord.lon}&appid=${process.env.WEATHER_API_KEY}
-&exclude=minutely&units=metric`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${city.coord.lat}&lon=${city.coord.lon}&appid=${process.env.WEATHER_API_KEY}&exclude=minutely&units=metric`;
   // Fetch the weather data
   const res = await fetch(url);
   const weatherData: WeatherData = await res.json();
@@ -33,22 +34,46 @@ type Props = {
 };
 
 export default function ({ city, weather }: Props) {
+  const iconUrl = `https://openweathermap.org/img/wn/$
+{weather.weather[0].icon}@2x.png`;
   return (
     <>
       <Head>
-        <title>WeatherView</title>
+        <title>Weather View</title>
       </Head>
-      <main>
-        <div className="container">
-          <Link href="/">&larr; Home</Link>
-          <h1>
-            {city.name} ({city.country})
-          </h1>
-          <h2>
-            {weather.main.temp_max.toFixed(0)}&deg;C
-            {weather.main.temp_min.toFixed(0)}&deg;C
-          </h2>
-          <div>{weather.weather[0].description}</div>
+      <main className="mt-5 mx-5">
+        <h1 className="text-xl font-medium mb-4">Weather View</h1>
+        <Link href="/" className="text-sm">
+          &larr; Home
+        </Link>
+        <div className="py-5">
+          <div className="bg-blue-500 rounded p-4">
+            <div className="grid grid-cols-2">
+              <div>
+                <h2 className="text-2xl mb-4 text-white">
+                  {city.name}({city.country})
+                </h2>
+                <span className="font-medium text-lg text-white">
+                  {weather.main.temp_max.toFixed(0)}&deg;C
+                </span>
+                &nbsp;
+                <span className="text-gray-300 text-sm">
+                  {weather.main.temp_min.toFixed(0)}&deg;C
+                </span>
+              </div>
+              <div className="justify-self-end">
+                <Image
+                  src={iconUrl}
+                  width={50}
+                  height={50}
+                  alt="Weather Icon"
+                />
+                <div className="text-white text-sm">
+                  {weather.weather[0].description}
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </main>
     </>
